36fad635bde3a5006887a2dc35b56dea5d6622cb
Gilbert <gilbert.tan1991@gmail.com>
2025-03-07 00:21:04 +0800

    Creating isset for the login

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once '/../config/database.php';
+
+//Check if the admin is logged in
+if(!isset($_SESSION['admin'])){
+    header('Location: login.php');
+    exit();
+} else {
+    header('Location: dashboard.php');
+    exit();
+}
+
+?>

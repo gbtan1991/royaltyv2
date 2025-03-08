750d48d4afe4156eb20d2c211a2e0364cc882212
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-03-08 17:14:48 +0800

    Done creating views page for adding admin and also created the proper structure for admin table

---
+++ views/add_admin.php
@@ -0,0 +1,40 @@
+<?php 
+require_once __DIR__ . "/../config/database.php";
+
+// if(!isset($_SESSION['admin'])){
+//     header('Location: ../public/login.php');
+//     exit();
+// }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Admin</title>
+</head>
+<body>
+
+    <h2> Add Admin </h2>
+
+    <form action="../controllers/add_admin.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br><br>
+        <select name="role">
+            <option value="admin">Admin</option>
+            <option value="superadmin">Super Admin</option>
+        </select>
+
+        <button type="submit">Add</button>
+
+</form>
+
+    <a href="../public/dashboard.php">Back to Dashboard</a>"
+    
+</body>
+</html>

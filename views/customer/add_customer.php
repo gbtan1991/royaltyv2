834f072f414a9d355ae9f098c6f8634e5e382697
Gilbert <gilbert.tan1991@gmail.com>
2025-03-20 00:38:52 +0800

    Changed the form

--- views/customer/add_customer.php
+++ views/customer/add_customer.php
@@ -16,11 +16,21 @@ require_once __DIR__ . "/../../config/database.php";
 
 <h2>Register New Customer</h2>
 
-    <form action="../../controllers/customer/add_customer.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required><br><br>
-        <label for="fullname">Full name:</label>
-        <input type="text" name="fullname" id="fullname" required><br><br>
+<form action="../../controllers/customer/add_customer.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username" required><br><br>
+
+    <label for="fullname">Full name:</label>
+    <input type="text" name="fullname" id="fullname" required><br><br>
+
+    <label>Gender:</label><br>
+    <input type="radio" name="gender" id="gender-male" value="Male" required>
+    <label for="gender-male">Male</label>
+    <input type="radio" name="gender" id="gender-female" value="Female">
+    <label for="gender-female">Female</label><br><br>
+    
+    <label for="birthdate">Birthdate:</label>
+    <input type="date" name="birthdate" id="birthdate" required><br><br>
 
         <button type="submit">Add</button>
     </form>

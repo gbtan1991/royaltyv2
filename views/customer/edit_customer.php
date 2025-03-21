24bd9651ce0e1c9e2d9ee2e5e2174bf927de2de0
Gilbert <gilbert.tan1991@gmail.com>
2025-03-21 14:08:11 +0800

    Created the form that will post to controllers/customer/edit_customer.php

---
+++ views/customer/edit_customer.php
@@ -0,0 +1,41 @@
+<?php 
+require_once __DIR__ . '/../../config/session.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Customer</title>
+</head>
+<body>
+
+    <h2>Edit Customer</h2>
+
+    <?php if (isset($_GET['error'])): ?>
+        <p style="color: red;"><?= htmlspecialchars($_GET['error']) ?></p>
+    <?php endif; ?>
+
+    <form action="../../controllers/customer/edit_customer.php" method="post">
+        <input type="hidden" name="id" value="<?= htmlspecialchars($customer['id']) ?>">
+
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" value="<?= htmlspecialchars($customer['username']) ?>" required><br><br>
+
+        <label for="fullname">Full Name:</label>
+        <input type="text" name="fullname" id="fullname" value="<?= htmlspecialchars($customer['fullname']) ?>" required><br><br>
+
+        <label>Gender:</label><br>
+        <input type="radio" name="gender" value="Male" <?= ($customer['gender'] == 'Male') ? 'checked' : '' ?> required> Male
+        <input type="radio" name="gender" value="Female" <?= ($customer['gender'] == 'Female') ? 'checked' : '' ?>> Female
+        <br><br>
+
+        <label for="birthdate">Birthdate:</label>
+        <input type="date" name="birthdate" id="birthdate" value="<?= htmlspecialchars($customer['birthdate']) ?>" required><br><br>
+
+        <button type="submit">Save Changes</button>
+    </form>
+
+</body>
+</html>

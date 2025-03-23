39664244e4e0374208ba467656f09e8ba386c54f
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-03-23 17:40:47 +0800

    Created database and module for the transaction

---
+++ models/Transaction.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once __DIR__ . '/../config/database.php';
+
+class Transaction {
+    private $pdo;
+
+
+    public function __construct(){
+        $this->pdo = $pdo;
+    }
+
+
+
+    
+}

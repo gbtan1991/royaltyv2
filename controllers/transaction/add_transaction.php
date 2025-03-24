cc51ebd40c115721ffc4b97c43491739189d3589
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-03-24 18:53:59 +0800

    Created a transaction call in adding transaction

--- controllers/transaction/add_transaction.php
+++ controllers/transaction/add_transaction.php
@@ -8,6 +8,20 @@ if (!isset($_SESSION['admin'])) {
     exit();
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
+    $customer_id = $_POST['customer_id'];
+    $admin_id = $_SESSION['admin']['id'];
+    $total_amount = $_POST['total_amount'];
 
+    $transactionModel = new Transaction($pdo);
+
+    if ($transactionModel->addTransactions($customer_id, $total_amount, $admin_id)) {
+        echo json_encode(["success" => true, "message" => "Transaction added successfully"];
+        ) else {
+        echo json_encode(["success" => false, "message" => "Transaction failed to add"];)
+        }
+    }
+
+}
 
 

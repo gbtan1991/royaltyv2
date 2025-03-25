cf6ed6d0207ab5d7be03f9152a30a3b1052e5bc2
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-03-25 19:32:34 +0800

    Removed the add transaction and refactor the get all transaction

--- models/Transaction.php
+++ models/Transaction.php
@@ -1,42 +1,28 @@
-<?php 
-
+<?php
 require_once __DIR__ . '/../config/database.php';
 
 class Transaction {
     private $pdo;
 
-
-    public function __construct($pdo){
+    public function __construct($pdo) {
         $this->pdo = $pdo;
     }
 
-    // Add a new transaction
-    public function addTransactions($customer_id, $admin_id, $total_amount){
-        $stmt = $this->pdo->prepare("INSERT INTO transaction (customer_id, admin_id, total_amount) VALUES (?, ?, ?)");
-        $stmt->execute([$customer_id, $admin_id, $total_amount]);
-
-        // Calculating points (5 pesos = 30 minutes = 1 point)
-        $points = floor($total_amount / 5);
-
-        // Update customer points
-        $stmt = $this->pdo->prepare("UPDATE customer SET total_points = total_points + ? WHERE id =");
-        return $stmt->execute([$points, $customer_id]);
-    }
-
-    // Fetch all transactions
+        
     public function getAllTransactions() {
-        $stmt = $this->pdo->query("
-            SELECT t.id, 
-            c.username AS customer, 
-            a.username AS admin,
-            t.total_amount,
-            t.transaction_date 
+        $stmt = $this->pdo->query(
+            "SELECT 
+                t.id,
+                c.username as customer_username,
+                a.username as admin_username,
+                t.total_amount,
+                t.transaction_date
             FROM transaction t
             JOIN customer c ON t.customer_id = c.id
             JOIN admin a ON t.admin_id = a.id
-            ORDER BY t.transaction_date DESC
-            ");
+            ORDER BY t.transaction_date DESC"            
+        );
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
 }
-    

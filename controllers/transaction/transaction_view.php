6273a7d9555edbb47c871b4ed535051417472473
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-03-25 19:33:12 +0800

    Create the controller transaction view

---
+++ controllers/transaction/transaction_view.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../models/Transaction.php';
+
+
+$transactionModel = new Transaction($pdo);
+$transactions = $transactionModel->getAllTransactions();
+
+
+require_once __DIR__ . '/../../views/transaction/transaction_view.php';

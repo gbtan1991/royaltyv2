6c3a9e3a7bfc4c2afac714bf383dddf3e4630db1
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-03-26 21:27:04 +0800

    Testing the modification of the  session

--- config/session.php
+++ config/session.php
@@ -1,5 +1,11 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
+
+    // Example: Storing admin session after login
+    $_SESSION['admin'] = [
+        'id' => $admin_id, 
+        'username' => $admin_username
+    ];
 }
 ?>

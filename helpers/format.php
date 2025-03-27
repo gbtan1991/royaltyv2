6da93b428f4334ca5176bd7cc69a6fcf0a2f4cc6
Gilbert <gilbert.tan1991@gmail.com>
2025-03-27 19:37:11 +0800

    Created formatHoursFromPoints for better display.

--- helpers/format.php
+++ helpers/format.php
@@ -15,3 +15,11 @@ function formatGender($gender) {
 function formatShortDate($dateTime) {
     return date('F j, Y', strtotime($dateTime)); 
 }
+
+
+function formatHoursFromPoints($totalPoints) {
+    $hours = floor($totalPoints / 2); // Each 2 points = 1 hour
+    $minutes = ($totalPoints % 2) * 30; // Remaining points converted to minutes
+
+    return sprintf("%d hr %d min", $hours, $minutes);
+}

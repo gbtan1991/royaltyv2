f65c63640864232bd22536be31b3f499ef94d4d8
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-01 21:45:09 +0800

    Done creating the reward model

---
+++ models/Reward.php
@@ -0,0 +1,46 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+
+class Reward {
+    private $pdo;
+
+    // Constructor to automactically use the database connection
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    // Fetch all rewards
+    public function getAllRewards() {
+        $stmt = $this->pdo->query("SELECT * FROM reward ORDER BY id ASC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    // Add a new reward
+    public function addReward($reward_name, $required_points, $reward_description) {
+        $stmt = $this->pdo->prepare("INSERT INTO reward (reward_name, required_points, reward_description) VALUES (?, ?, ?)");
+        return $stmt->execute([$reward_name, $required_points, $reward_description]);
+    }
+
+    // Fetch reward by ID
+    public function getRewardById($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM reward WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Update reward
+    public function updateReward($id, $reward_name, $required_points, $reward_description) {
+        $stmt = $this->pdo->prepare("UPDATE reward SET reward_name = ?, required_points = ?, reward_description = ?, modified_at = NOW() WHERE id = ?");
+        return $stmt->execute([$id, $reward_name, $required_points, $reward_description]);
+    }
+
+    public function deleteReward($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM reward WHERE id = ?");
+        return $stmt->execute([$id]);
+    }
+    
+}
+
+
+

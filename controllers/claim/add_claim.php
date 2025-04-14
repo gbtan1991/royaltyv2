025a6d568ef01d1a3ebf5884e857c7f13879c260
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-14 20:16:26 +0800

    Created the add_claim controller.

--- controllers/claim/add_claim.php
+++ controllers/claim/add_claim.php
@@ -1,2 +1,77 @@
 <?php 
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../config/session.php';
+require_once __DIR__ . '/../../models/Claim.php';
+require_once __DIR__ . '/../../models/Customer.php';
+require_once __DIR__ . '/../../models/Reward.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: ../../views/claim/add_claim.php?error=Invalid Request Method');
+    exit();
+
+}
+
+// Validate required fields
+
+$reward_id = $_POST['reward_id'] ?? null;
+$customer_id = $_POST['customer_id'] ?? null;
+$remarks = $_POST['remarks'] ?? null;
+$admin_id = $_SESSION['admin_id'] ?? null;
+
+
+
+if (!$reward_id || !$customer_id || !$remarks || !$admin_id) {
+    header('Location: ../../views/claim/add_claim.php?error=Mising required data');
+}
+
+// Instance models
+
+$rewardModel = new Reward($pdo);
+$customerModel = new Customer($pdo);
+$claimModel = new Claim($pdo);
+
+
+// Get reward Details
+
+$reward = $rewardModel->getRewardById($reward_id);
+$customer = $customerModel->getCustomerById($customer_id);
+
+if (!$reward || !$customer) {
+    header('Location: ../../views/claim/add_claim.php?error=Invalid Reward or Customer ID');
+    exit;
+}
+
+//  Check if customer has enough points
+if ($customer['total_points'] < $reward['required_points']) {
+    header('Location: ../../views/claim/add_claim.php?error=Not enought points to claim this reward');
+    exit;
+}
+
+
+try {
+    // Start transaction
+    $pdo->beginTransaction();
+
+
+    //Insert claim
+    $claimSuccess = $claimModel->addClaim($customer_id, $admin_id, $reward_id, $reward['required_points'], $remarks ?? '');
+
+    $updatePoints = $customerModel->deductPoints($customer_id, $reward['required_points']);
+
+    if ($claimSuccess && $updatePoints) {
+        $pdo->commit();
+        header('Location: ../../views/claim/add_claim.php?success=Claim added successfully');
+    } else {
+        $pdo->rollBack();
+        header('Location: ../../views/claim/add_claim.php?error=Failed to add claim');
+    }
+
+    exit;
+} catch (Exception $e) {
+    $pdo->rollback();
+    header('Location: ../../views/claim/add_claim.php?error=An error occurred: ' . $e->getMessage());
+    exit;
+}
+
 

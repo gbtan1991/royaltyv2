35ef9cb22be56d6162b6a24cbc3611c7d90dbb30
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-19 19:58:02 +0800

    Added the styling div tags

--- public/login.php
+++ public/login.php
@@ -19,20 +19,21 @@ if (isset($_SESSION['admin'])) {
 </head>
 <body>
 
-<div class="login-page-container">
-    <div class="login-container">
-    <h2>Royalty - Admin Login</h2>
+<div class="login-container">
+    <div class="container">
+    <img src="../assets/image/royalty-logo.png" alt="Royalty Logo" class="logo">
+    <h2>Admin Login</h2>
 
     <?php if (isset($_GET['error'])): ?>
         <p class="error"><?= htmlspecialchars($_GET['error']) ?></p>
     <?php endif; ?>
 
-    <form action="../controllers/auth.php" method="post">
+    <form action="../controllers/auth.php" method="post" class="form-login">
         <input type="text" name="username" placeholder="Enter Username" required>
         <input type="password" name="password" placeholder="Enter Password" required>
-        <button type="submit">Login</button>
+        <button type="submit" >Login</button>
     </form>
-</div>
+    </div>
 </div>
 </body>
 </html>

e74218326c47a5d83adfbf2025d5b75ce9995236
Gilbert <gilbert.tan1991@gmail.com>
2025-04-26 01:21:57 +0800

    Created a footer with timer and copyright

---
+++ partials/footer.php
@@ -0,0 +1,16 @@
+    <div>
+        <footer class="footer">
+            <p>&copy; <?= date('Y') ?> Royalty App. All rights reserved.</p>
+            <div class="date-time">
+                <p id="currentDate">1</p>
+                <p id="currentTime">1</p>
+                
+            </div>
+        </footer>
+    </div>
+
+    
+    <script src="../assets/js/<?= htmlspecialchars($currentPage) ?>.js"></script>
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>

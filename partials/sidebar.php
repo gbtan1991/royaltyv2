ea02f26fc818efb440193bf419341e91c02a8bdf
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-25 20:52:33 +0800

    Since I've created a separate reusable sidebar. I've changed the sidebar header

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,5 +1,12 @@
+
+
+
+
+
 <aside class="sidebar">
     <div class="sidebar-header">
-        <img src="../assets/image/royalty-logo.php alt="Royalty logo" class="sidebar-logo">
+        <img src="../assets/image/royalty-logo.png" alt="Royalty logo" class="sidebar-logo">
+        <h1>Royalty</h1>
+        <p>Rewards Application</p>
     </div>
 </aside>

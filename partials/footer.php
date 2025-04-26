e068c629376ab50a0f2ebd0183494f6a82ca96e4
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-26 20:45:05 +0800

    Changes the initial textContent of the date and time.

--- partials/footer.php
+++ partials/footer.php
@@ -2,8 +2,8 @@
         <footer class="footer">
             <p>&copy; <?= date('Y') ?> Royalty App. All rights reserved.</p>
             <div class="date-time">
-                <p id="currentDate">1</p>
-                <p id="currentTime">1</p>
+                <p id="currentDate">:</p>
+                <p id="currentTime">:</p>
                 
             </div>
         </footer>

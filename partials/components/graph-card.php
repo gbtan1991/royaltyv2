186a1335f836e60e22d04523a96a88106a2cdbd1
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-28 21:42:14 +0800

    Created the graph-card for the reusable graph.

---
+++ partials/components/graph-card.php
@@ -0,0 +1,8 @@
+
+    <div class="graph-card">
+    <h3><?= htmlspecialchars($chart['title']) ?></h3>
+
+    <canvas id="<?= htmlspecialchars($chart['chartId']) ?>">
+         </canvas>
+
+    </div>  

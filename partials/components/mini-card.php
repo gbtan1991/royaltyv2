6b15780ee90aeaf117574292425faeca6a5453d9
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-04-30 19:36:19 +0800

    Adjusted the minimal issues like displaying of currency and making default 0 if theres nothing fetch from the database

--- partials/components/mini-card.php
+++ partials/components/mini-card.php
@@ -1,11 +1,16 @@
-<?php if(isset($label, $content, $logo)) : ?>
+<?php if(isset($label, $logo)) : ?>
     <div class="mini-card">
         <?php if (($iconPosition ?? 'left') === 'left'): ?>
             <i class="<?= htmlspecialchars($logo) ?>"></i>
         <?php endif; ?>
         <div class="mini-card-content">
             <h3><?= htmlspecialchars($label) ?></h3>
-            <p><?= htmlspecialchars($content) ?></p>
+            <p>
+                <?php
+                    $value = $content ?? 0;
+                    echo $isCurrency ? 'P ' . htmlspecialchars((string)$value) : htmlspecialchars((string)$value);
+                ?>
+            </p>   
         </div>
         <?php if (($iconPosition ?? 'left') === 'right'): ?>
             <i class="<?= htmlspecialchars($logo) ?>"></i>

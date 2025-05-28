3d2db0dfabd658d1d3aa2cc7a45f40123bcd73aa
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-05-28 15:42:56 +0800

    Created a menu-button-component that will include by the buttons array

---
+++ partials/components/menu-button-component.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../../config/config.php';
+
+$basePublicUrl = BASE_URL . 'public/';
+
+// Define buttons with the same structure as sidebar
+
+?>
+
+<div class="button-menu">
+    <?php foreach ($buttons as $btn): ?>
+        <?php
+            $href = $basePublicUrl . 'index.php?page=' . $btn['page'];
+            $label = $btn['label'];
+            $logo = $btn['logo'] ?? '';
+        ?>
+        <a href="<?= $href ?>" >
+            
+                <i class="<?= $logo ?>"></i> <?= $label ?>
+        
+        </a>
+    <?php endforeach; ?>
+</div>

9ff2e07082cfeac5825a23c0cc401cfce675740e
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-05-28 21:33:00 +0800

    Editted the buttons array with add customer.

---
+++ partials/components/buttons/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$buttons = [
+    ['page' => 'add_customer', 'label' => 'Register New Customer', 'logo' => 'fa-solid fa-user-plus'],
+    ['page' => 'transaction_add', 'label' => 'Add New Transaction', 'logo' => 'fa-solid fa-square-plus'],
+    ['page' => 'reward_add', 'label' => 'Customer Claim Rewards', 'logo' => 'fa-solid fa-certificate'],
+    ['page' => 'claim_add', 'label' => 'Add New Reward', 'logo' => 'fa-solid fa-gift'],
+];
+
+include __DIR__ . '../../menu-button-component.php';

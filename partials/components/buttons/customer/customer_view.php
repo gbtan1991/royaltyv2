2ac0e6242083757a6b675ae0f21af59040be9f9c
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-05-28 21:34:02 +0800

    Added the buttons array for the customer_view.

---
+++ partials/components/buttons/customer/customer_view.php
@@ -0,0 +1,8 @@
+<?php 
+
+    $buttons = [
+        ['page' => 'add_customer' , 'label' => 'Register New Customer', 'logo' => 'fa-solid fa-user-plus'],
+        ['page' => 'dashboard', 'label' => 'Back to Dashboard', 'logo' => 'fa-solid fa-arrow-up-right-from-square'],
+    ];
+
+    include __DIR__ . '../../../menu-button-component.php';

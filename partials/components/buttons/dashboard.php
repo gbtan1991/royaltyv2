12751f8cccfa0ef83f1afb5a0769912dc262804a
gilbert <gilbert.tan1991@gmail.com>
2025-05-31 12:29:10 +0800

    Changing the text and page link for the dashboard buttons

--- partials/components/buttons/dashboard.php
+++ partials/components/buttons/dashboard.php
@@ -3,8 +3,8 @@
 
 $buttons = [
     ['page' => 'add_customer', 'label' => 'Register New Customer', 'logo' => 'fa-solid fa-user-plus'],
-    ['page' => 'transaction_add', 'label' => 'Add New Transaction', 'logo' => 'fa-solid fa-square-plus'],
-    ['page' => 'reward_add', 'label' => 'Customer Claim Rewards', 'logo' => 'fa-solid fa-certificate'],
+    ['page' => 'add_transaction', 'label' => 'Add New Transaction', 'logo' => 'fa-solid fa-square-plus'],
+    ['page' => 'reward_add', 'label' => 'Add New Customer Claim', 'logo' => 'fa-solid fa-certificate'],
     ['page' => 'claim_add', 'label' => 'Add New Reward', 'logo' => 'fa-solid fa-gift'],
 ];
 

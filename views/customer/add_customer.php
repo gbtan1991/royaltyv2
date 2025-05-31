00efd8e6568a71d9863a0d7245dd432adf7b9d60
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-05-31 20:31:32 +0800

    Removed the excessive tags.

--- views/customer/add_customer.php
+++ views/customer/add_customer.php
@@ -1,19 +1,8 @@
 <?php
 require_once __DIR__ . '/../../config/database.php';
 
-
-
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register New Customer</title>
-</head>
-<body>
 
 <h2>Register New Customer</h2>
 
@@ -44,5 +33,3 @@ require_once __DIR__ . '/../../config/database.php';
     <button type="submit">Add</button>
     <a href="index.php?page=customer_view">Cancel</a>
 </form>
-</body>
-</html>

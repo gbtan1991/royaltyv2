1d8b9abbf634cc6012908c5e6a3f7b0a33b8bf7d
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-02 21:26:59 +0800

    Created styling for edit_customer

--- views/customer/edit_customer.php
+++ views/customer/edit_customer.php
@@ -2,16 +2,13 @@
 require_once __DIR__ . '/../../config/session.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Customer</title>
-</head>
-<body>
+<div class="edit-customer-page">
 
-    <h2>Edit Customer</h2>
+
+<h2>Edit Customer</h2>
+
+<div class="notification">
+
 
     <?php if(isset($_GET['success'])): ?>
         <p style="color: green;"><?= htmlspecialchars($_GET['success']) ?></p>
@@ -20,25 +17,49 @@ require_once __DIR__ . '/../../config/session.php';
         <p style="color: red;"><?= htmlspecialchars($_GET['error']) ?></p>
     <?php endif; ?>
 
-    <form action="index.php?page=update_customer" method="post">
-        <input type="hidden" name="id" value="<?= htmlspecialchars($customer['id']) ?>">
+</div>
 
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" value="<?= htmlspecialchars($customer['username']) ?>" required><br><br>
+    <form action="index.php?page=update_customer" method="post">
+        
+        <input type="hidden" name="id" value="<?= htmlspecialchars($customer['id']) ?>">
+        
+        <div class="label-field">            
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username" value="<?= htmlspecialchars($customer['username']) ?>" required><br><br>
+        </div>
+
+        <div class="label-field">
 
         <label for="fullname">Full Name:</label>
         <input type="text" name="fullname" id="fullname" value="<?= htmlspecialchars($customer['fullname']) ?>" required><br><br>
 
-        <label>Gender:</label><br>
-        <input type="radio" name="gender" value="Male" <?= ($customer['gender'] == 'Male') ? 'checked' : '' ?> required> Male
-        <input type="radio" name="gender" value="Female" <?= ($customer['gender'] == 'Female') ? 'checked' : '' ?>> Female
-        <br><br>
+        </div>
 
+        <div class="info-fields">
+        <div class="gender-options">
+
+        <label>Gender:</label><br>
+
+        <label for="gender-male">
+            <input type="radio" name="gender" value="Male" <?= ($customer['gender'] == 'Male') ? 'checked' : '' ?> required> Male <i class="fa-solid fa-mars"></i>
+        </label>
+
+        <label for="gender-female">
+            <input type="radio" name="gender" value="Female" <?= ($customer['gender'] == 'Female') ? 'checked' : '' ?>> Female <i class="fa-solid fa-venus"></i>
+        </label>
+
+        </div>
+
+        <div class="birthdate-field">
         <label for="birthdate">Birthdate:</label>
         <input type="date" name="birthdate" id="birthdate" value="<?= htmlspecialchars($customer['birthdate']) ?>" required><br><br>
+        </div>
+        </div>
+        <div class="button-fields">
+            <button type="submit"><i class="fa-solid fa-plus"></i><p>Save Changes</p></button>
+        <a href="index.php?page=customer_view"><i class="fa-solid fa-ban"></i><p>Cancel</p></a>
 
-        <button type="submit">Save Changes</button>
+        </div>
     </form>
 
-</body>
-</html>
+    </div>

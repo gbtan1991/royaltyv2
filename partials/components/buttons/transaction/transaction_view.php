555b4b5a8ed2d01f9eedc03a5a539c2ddd93c1d4
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-04 21:19:01 +0800

    created buttons component with the correct page and label

---
+++ partials/components/buttons/transaction/transaction_view.php
@@ -0,0 +1,8 @@
+<?php 
+
+    $buttons = [
+        ['page' => 'add_transaction' , 'label' => 'Add New Transaction', 'logo' => 'fa-solid fa-plus'],
+        ['page' => 'dashboard', 'label' => 'Back to Dashboard', 'logo' => 'fa-solid fa-arrow-up-right-from-square'],
+    ];
+
+    include __DIR__ . '../../../menu-button-component.php';

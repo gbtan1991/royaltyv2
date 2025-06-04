d8f2c9e74dcb348bc3d7d559daa5d82fd46a2d88
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-04 21:18:39 +0800

    removed excessive admin checks.

--- controllers/transaction/transaction_view.php
+++ controllers/transaction/transaction_view.php
@@ -1,7 +1,13 @@
 <?php 
 
 require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../config/session.php';
 require_once __DIR__ . '/../../models/Transaction.php';
+
+if(!isset($_SESSION['admin'])){
+    header('Location: ../../public/login.php');
+    exit();
+}
 
 
 

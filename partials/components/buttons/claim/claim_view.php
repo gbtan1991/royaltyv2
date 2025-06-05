306df7d53266fa570d658ff2d61004179b2a1e52
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-05 19:50:08 +0800

    Added the buttons on the top for claim view.

---
+++ partials/components/buttons/claim/claim_view.php
@@ -0,0 +1,8 @@
+<?php 
+
+    $buttons = [
+        ['page' => 'add_claim' , 'label' => 'Claim Rewards', 'logo' => 'fa-solid fa-plus'],
+        ['page' => 'dashboard', 'label' => 'Back to Dashboard', 'logo' => 'fa-solid fa-arrow-up-right-from-square'],
+    ];
+
+    include __DIR__ . '../../../menu-button-component.php';

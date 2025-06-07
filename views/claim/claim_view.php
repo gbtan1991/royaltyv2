9d18b5f540ae573cd38738c51167408c246aeed8
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-07 21:38:42 +0800

    Removed the excessive tags. and preparing for styling.

--- views/claim/claim_view.php
+++ views/claim/claim_view.php
@@ -3,24 +3,7 @@ require_once __DIR__ . '/../../helpers/format.php';
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View CLaims</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-            padding: 8px;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
+
     <h2>List of Claims</h2>
     
     <?php if(isset($_GET['success'])): ?>
@@ -56,8 +39,7 @@ require_once __DIR__ . '/../../helpers/format.php';
                         <td><?= htmlspecialchars($claim['claim_date']) ?></td>
                         <td><?= htmlspecialchars($claim['claim_status']) ?></td>
                         <td><?= htmlspecialchars($claim['remarks']) ?></td>
-                        <td><a href="edit_claim.php?id=<?= htmlspecialchars($claim['id']) ?>">Edit</a></td>
-                        <td><a href="delete_claim.php?id=<?= htmlspecialchars($claim['id']) ?>" onclick="return confirm('Are you sure you want to delete this claim?');">Delete</a></td>
+                        <td><a href="index.php?page=delete_claim&id=<?= htmlspecialchars($claim['id']) ?>" onclick="return confirm('Are you sure you want to delete this claim?');">Delete</a></td>
 
                     </tr>
 
@@ -70,9 +52,4 @@ require_once __DIR__ . '/../../helpers/format.php';
         </thead>    
     
     </table>
-    <br>
-    <a href="../../views/claim/add_claim.php">Add New Claim</a>
-    <a href="../../views/dashboard.php">Back to Dashboard</a>
-
-</body>
-</html>
+   

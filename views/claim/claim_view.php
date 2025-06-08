d091549ef4693dd7a43f4ef8f56c25574206adfd
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-08 22:15:44 +0800

    Create a propert layout sync with its styling/sass

--- views/claim/claim_view.php
+++ views/claim/claim_view.php
@@ -2,16 +2,17 @@
 require_once __DIR__ . '/../../helpers/format.php';
 ?>
 
-
-
+<div class="claim-view-layout">
     <h2>List of Claims</h2>
-    
     <?php if(isset($_GET['success'])): ?>
         <p style="color: green;"><?= htmlspecialchars($_GET['success']) ?></p>
     <?php endif; ?>
     <?php if(isset($_GET['error'])): ?>
         <p style="color: red;"><?= htmlspecialchars($_GET['error']) ?></p>
     <?php endif; ?>
+
+<input type="text" id="search-claim" placeholder="Search Claim">
+
 
     <table>
         <thead>
@@ -27,19 +28,19 @@ require_once __DIR__ . '/../../helpers/format.php';
             <th colspan="2">Actions</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody id="claim-table-body">
             <?php if(!empty($claims)): ?>
                 <?php foreach ($claims as $claim) : ?>
                     <tr>
                         <td><?= htmlspecialchars($claim['id']) ?></td>
                         <td><?= htmlspecialchars($claim['customer_username']) ?></td>
-                        <td><?= htmlspecialchars($claim['admin_username']) ?></td>
+                        <td><?= formatAdmin($claim['admin_username']) ?></td>
                         <td><?= htmlspecialchars($claim['reward_name']) ?></td>
                         <td><?= htmlspecialchars($claim['points_used']) ?></td>
                         <td><?= htmlspecialchars($claim['claim_date']) ?></td>
                         <td><?= htmlspecialchars($claim['claim_status']) ?></td>
                         <td><?= htmlspecialchars($claim['remarks']) ?></td>
-                        <td><a href="index.php?page=delete_claim&id=<?= htmlspecialchars($claim['id']) ?>" onclick="return confirm('Are you sure you want to delete this claim?');">Delete</a></td>
+                        <td class="table-actions"><a href="index.php?page=delete_claim&id=<?= htmlspecialchars($claim['id']) ?>" class="delete-button" onclick="return confirm('Are you sure you want to delete this claim?');"><i class="fa-solid fa-trash"></i></a></td>
 
                     </tr>
 
@@ -53,3 +54,11 @@ require_once __DIR__ . '/../../helpers/format.php';
     
     </table>
    
+
+</div>
+    
+<div id="pagination-controls">
+    <button id="prev-page" class="pagination"
+
+
+</div>

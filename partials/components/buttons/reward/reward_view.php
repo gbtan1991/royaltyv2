45912299d13d2b6cde2b7a6b0861520e9b0ac80e
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-16 21:43:51 +0800

    Created the button for reward_view

--- partials/components/buttons/reward/reward_view.php
+++ partials/components/buttons/reward/reward_view.php
@@ -1,3 +1,8 @@
 <?php 
 
-echo "test";
+    $buttons = [
+        ['page' => 'add_reward' , 'label' => 'Add New Reward', 'logo' => 'fa-solid fa-gift'],
+        ['page' => 'dashboard', 'label' => 'Back to Dashboard', 'logo' => 'fa-solid fa-arrow-up-right-from-square'],
+    ];
+
+    include __DIR__ . '../../../menu-button-component.php';

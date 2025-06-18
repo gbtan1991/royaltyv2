673f789fc29ca68d0ff96c3347726bb1202a1056
Gilbert Tan <gilbert.tan1991@gmail.com>
2025-06-18 21:08:54 +0800

    Changed the links for according to the routes.

---
+++ partials/components/buttons/admin/admin_view.php
@@ -0,0 +1,8 @@
+<?php 
+
+    $buttons = [
+        ['page' => 'add_admin' , 'label' => 'Add new Admin', 'logo' => 'fa-solid fa-user-plus'],
+        ['page' => 'dashboard', 'label' => 'Back to Dashboard', 'logo' => 'fa-solid fa-arrow-up-right-from-square'],
+    ];
+
+    include __DIR__ . '../../../menu-button-component.php';
--- partials/components/buttons/dashboard.php
+++ partials/components/buttons/dashboard.php
@@ -4,8 +4,8 @@
 $buttons = [
     ['page' => 'add_customer', 'label' => 'Register New Customer', 'logo' => 'fa-solid fa-user-plus'],
     ['page' => 'add_transaction', 'label' => 'Add New Transaction', 'logo' => 'fa-solid fa-square-plus'],
-    ['page' => 'reward_add', 'label' => 'Add New Customer Claim', 'logo' => 'fa-solid fa-certificate'],
-    ['page' => 'claim_add', 'label' => 'Add New Reward', 'logo' => 'fa-solid fa-gift'],
+    ['page' => 'add_claim', 'label' => 'Add New Customer Claim', 'logo' => 'fa-solid fa-certificate'],
+    ['page' => 'add_reward', 'label' => 'Add New Reward', 'logo' => 'fa-solid fa-gift'],
 ];
 
 include __DIR__ . '../../menu-button-component.php';

570205b45c2189e439dba07871ef298757101abf
Gilbert Tan <gilbert.tan1991@gmail.com>
2026-04-13 21:50:11 +0800

    Started creating the Database Connection

---
+++ app/Core/Database.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (self::$instance === null) {
+            try {
+                $dsn = "mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'] . ";charset=utf8mb4";
+                self::$instance = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]);
+            } catch (PDOException $e) {
+                die("Database Error: " . $e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
+}

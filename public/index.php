f0ae713d2bbd9bb20fabe7585e99a0b01c46a57e
Gilbert Tan <gilbert.tan1991@gmail.com>
2026-04-13 23:09:09 +0800

    Done prototyping the index.php to fully see that the database is communicating well with the project structure.

--- public/index.php
+++ public/index.php
@@ -3,17 +3,35 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Bramus\Router\Router;
+use App\Models\Customer;
 
-// Load environment
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
 $router = new Router();
 
-// Test Route
 $router->get('/', function() {
-    echo "<h1>Royalty V2: Modern Edition</h1>";
-    echo "<p>PSR-4 Autoloading is working!</p>";
+    $customerModel = new Customer();
+    $customers = $customerModel->all();
+
+    echo "<h1>Royalty V2: Customer List</h1>";
+    
+    if (empty($customers)) {
+        echo "<p>No customers found. Try adding some to your database!</p>";
+    } else {
+        echo "<table border='1' cellpadding='10' style='border-collapse: collapse;'>";
+        echo "<thead><tr><th>ID</th><th>Username</th><th>Full Name</th><th>Points</th></tr></thead>";
+        echo "<tbody>";
+        foreach ($customers as $user) {
+            echo "<tr>";
+            echo "<td>{$user['id']}</td>";
+            echo "<td>{$user['username']}</td>";
+            echo "<td>{$user['fullname']}</td>";
+            echo "<td><strong>{$user['total_points']}</strong></td>";
+            echo "</tr>";
+        }
+        echo "</tbody></table>";
+    }
 });
 
 $router->run();

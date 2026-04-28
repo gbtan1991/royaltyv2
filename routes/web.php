fffc761f9536805dc976e4a61957c3e479be0823
Gilbert Tan <gilbert.tan1991@gmail.com>
2026-04-28 22:01:31 +0800

    Created the routes for the customer

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Helpers\Route;
 use App\Controllers\admin\AdminController;
+use App\Controllers\customer\CustomerController;
+use App\Helpers\Route;
 
 /**
  * Admin Routes
@@ -16,3 +17,17 @@ Route::get('/admin/show/(\d+)', [AdminController::class, 'show']);
 Route::get('/admin/edit/(\d+)', [AdminController::class, 'edit']);
 Route::post('/admin/update/(\d+)', [AdminController::class, 'update']);
 Route::post('/admin/destroy/(\d+)', [AdminController::class, 'destroy']);
+
+
+/**
+ * Customer Routes
+ * We use the Route helper (Translator) instead of the $router variable
+ */
+
+// This now works because the Helper converts the array to a string!
+Route::get('/customer', [CustomerController::class, 'index']);
+Route::get('/customer/create', [CustomerController::class, 'create']);
+Route::post('/customer/store', [CustomerController::class, 'store']);
+Route::get('/customer/show/(\d+)', [CustomerController::class, 'show']);
+Route::get('/customer/edit/(\d+)', [CustomerController::class, 'edit']);
+Route::post('/customer/update/(\d+)', [CustomerController::class,'update']);
